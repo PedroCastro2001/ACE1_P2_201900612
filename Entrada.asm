ca67a5413fb19125da3ff87ee03e12f56130ab96
Pedro Castro <pedrocastro@HP-Laptop-14-ck0xxx-c1e6fec1.localdomain>
2022-12-24 09:43:36 -0600

    Documentando

--- Entrada.asm
+++ Entrada.asm
@@ -1,7 +1,11 @@
-org 100h
+org 100h ;Le indica al ensamblador que continúe el ensamblado a partir de la dirección 100H
 
-section .data
-    ;encabezado: db 0ah,"**************************************************** PROYECTO 2 FASE 1 ****************************************************",0,0ah,"Laboratorio Arquitectura de Computadoras y Ensambladores 1",0,0ah,"Pedro Antonio Castro Calderón",0,0ah,"201900612",0,0ah,"***************************************************************************************************************************"**************$",0,0ah,0ah
+section .data   ;definiendo la data antes de la compilación
+    ;db significa que serán bytes crudos
+    ;0ah es salto de linea
+    ;0dh es retorno de carro
+    ;$ significa que hasta ahí se termina la cadena, o en su lugar,colocar 24h fuera de la cadena
+    ;9h es un espacio de tabulación
     encabezado: db 0ah,"***************************** PROYECTO 2 FASE 1 *****************************",0,0ah,0ah,"Laboratorio Arquitectura de Computadoras y Ensambladores 1",0,0ah,"Pedro Antonio Castro Calderon",0,0ah,"201900612",0,0ah,0ah,"*****************************************************************************$"
     texto1 db 0ah,0ah,"Ingrese el numero de la opcion que desea, o pulse ESC para salir.",'$'
     menu db 0ah,0ah,9h,"(1) Ingresar los coeficientes de la funcion",0ah,0dh,9h,"(2) Imprimir la funcion almacenada",0ah,0dh,9h,"(3) Imprimir derivada de la funcion almacenada",0ah,0dh,9h,"(4) Imprimir integral de la funcion almacenada",0ah,0dh,9h,"(5) Graficar la funcion",0ah,0dh,9h,"(6) Metodo de Newton",0ah,0dh,9h,"(7) Metodo de Steffensen",0ah,0dh,9h,"(8) Metodo de Miller",0ah,0dh,9h,"(9) Salir de la aplicacion",0ah,0dh,24h
@@ -72,19 +76,20 @@ section .data
 
     Signo: db 2Dh, '$'
 
-section .bss
+section .bss ;Reservando bytes para su posterior uso
     opcion resb 2
     coefi5 resb 5
     coefi4 resb 6
     resultado resb 10
 
-section .text
+section .text   ;aquí empezará la ejecución
     global _start
 
 _start:
-    mov ah,00h              ;Limpiar la pantalla
-    mov al, 03h
-    int 10h
+    ;Limpiando pantalla
+    mov ah,00h              ;Poner el modo video
+    mov al, 03h             ;Obtener la posición y el tamaño del cursor
+    int 10h                 ;Interrupción del BIOS
 
     mov ah, 09h             ;Imprimir en pantalla lo que está en dx
     mov dx, encabezado
@@ -111,7 +116,7 @@ _getEntrada:
     mov ah, 01h             ;Lee un caracter ingresado por el usuario, y se almacena en el registro al
     mov al, opcion
     int 21h
-
+                            ;cmp: comparar dos operandos , si es igual se ejecuta el salto je
     cmp al, 31h             ;Si el caracter almacenado en al es 31h (1 en ascii) se ejecutará el método de ingresar función
     je _ingresarFuncion
 
@@ -143,13 +148,10 @@ _getEntrada:
     cmp al, 1bh             
     je _Escape
 
-    jmp _opcionInvalida
-
     ;Si no es ninguna, mostrará error por opción incorrecta.
+    jmp _opcionInvalida ;saltando a la funcion de opcion invalida
 
-
-    ret
-
+    ret     ;instrucción de retorno
 
 _ingresarFuncion:
     mov ah, 09h
@@ -170,35 +172,36 @@ _ingresarCoeficientes:
     mov ah, 09h
     mov dx, Ouput1      
     int 21h
+                    ;Leer signo + o -
+    mov ah, 01h     ;Leer un caractér del teclado y desplegarlo
+    int 21h         ;interrupción DOS
+    sub al, 30h     ;Al registro donde se guarda el caracter se le resta 30h (0 en hex) para que pueda ser manejado 
+    mov [s_x5], al  ;Ese valor ya se guarda en lo que contenga el registro para su posterior uso.
 
-    mov ah, 01h
+    mov ah, 01h         ;Se ingresa otro caracter 
     int 21h
-    sub al, 30h
-    mov [s_x5], al
+    cmp al,'0'          ;checa si el caracter es un digito
+    jb _noEsNumero      ;Si el digito es menos que 0, se ejecutará un error
+    cmp al, '9'         ;checa si el caracter es un digito
+    ja _noEsNumero      ;Si el digito es mayor que 9, se ejecutará un error
+    sub al, 30h         ;Si es un digito, se le resta 30h para poder ser manejado
+    mov [d_x5], al      ;El resultado se guarda en el contenido de la memoria en la direccipon d_x5
 
-    mov ah, 01h
-    int 21h
-    cmp al,'0'
-    jb _noEsNumero
-    cmp al, '9'
-    ja _noEsNumero
-    sub al, 30h
-    mov [d_x5], al
-
-    mov ah, 01h
+    mov ah, 01h         ;INgresando el digito de unidades.
     int 21h
     sub al, 30h
     mov [u_x5], al
 
     ;almacenando el número completo
-    mov al, [d_x5]
-    mov bl, 10
+    mov al, [d_x5]      ;Se realiza la multiplicación del digito de las decenas
+    mov bl, 10          ;x10
     mul bl
-    add al, [u_x5]
-    mov [n_x5], al
+    add al, [u_x5]      ;A esa multiplicación se le suman las unidades
+    mov [n_x5], al      ;Ya tenemos un número de 2 cifras que se guardará en n_x5
 
-    call _lineaNueva
-
+    call _lineaNueva    ;Llamando a función que imprime nueva linea
+                        
+    ;El procedimiento anterior se repite para los coeficientes restantes, únicamente cambia el lugar donde se almacenan los valores.                
     ;ingresando coeficiente x a la 4
     mov ah, 09h
     mov dx, Output2    
@@ -346,25 +349,26 @@ _ingresarCoeficientes:
 
     call _lineaNueva
 
-    mov ah, 09h
+    mov ah, 09h         ;Se imprimre mensaje en pantalla de que se almacenó la función
     mov dx, Output8
     int 21h 
 
     call _lineaNueva
     call _lineaNueva
 
-    mov ah, 01h
-    int 21h
-    cmp al, 30h             
-    je _start
+    mov ah, 01h         ;Espera a que el usuario ingrese un caracter
+    int 21h     
+    cmp al, 30h         ;Si el caracter ingresado es igual a 0 (30h en hex)            
+    je _start           ;Regresará al menú
 
     ret
 
 
 _verFuncion:
-    mov ah,00h              ;Limpiar la pantalla
-    mov al, 03h
-    int 10h
+    ;Limpiando pantalla
+    mov ah,00h              ;Poner el modo video
+    mov al, 03h             ;Obtener la posición y el tamaño del cursor
+    int 10h                 ;Interrupción del BIOS
 
     call _lineaNueva
 
@@ -375,28 +379,29 @@ _verFuncion:
     int 21h
 
     ;imprimiendo coeficiente x5
-    mov ah, 02h
-    mov dl, [s_x5]
-    add dl, 30h
-    int 21h
+    mov ah, 02h         ;nos despliega un caracter en la pantalla
+    mov dl, [s_x5]      ;que será el signo contenido en la dirección de memoria s_x5
+    add dl, 30h         ;pero antes.. se le sumarán 30h para que el usuario pueda verlo como signo
+    int 21h             ;interrupción DOS
 
-    mov al, [n_x5]
-    AAM
-    mov bx, ax
-    mov ah, 02h
-    mov dl, bh
-    add dl, 30h
-    int 21h
+    mov al, [n_x5]      ;moviendo lo que hay en el registro n_x5
+    AAM                 ;Instrucción de Desempaquetar
+    mov bx, ax          ;mover el registro ax en bx
+    mov ah, 02h         ;Desplegando un caracter en la pantalla, que será el primer dígito
+    mov dl, bh          ;Mover a dl lo que hay en dh
+    add dl, 30h         ;Sumando lo que hay en el registro dl un 30h para que el usuario pueda verlo como número
+    int 21h             ;Interrupción DOS
     
-    mov ah, 02h
-    mov dl, bl
-    add dl, 30h
-    int 21h
+    mov ah, 02h         ;Desplegará otro caracter en la pantalla, que será el segundo dígito
+    mov dl, bl          ;Moviendo al registro dl lo que hay en el registro b en su parte baja
+    add dl, 30h         ;Se le sumará 30h para poder verse como número decimal
+    int 21h             ;Interrupción DOS
 
     mov ah, 09h
     mov dx, Output9
     int 21h
 
+    ;El procedimiento mostrado arriba se repite para los demás coeficientes, cambiando unicamente los valores.
     ;imprimiendo coeficiente x4
     mov ah, 02h
     mov dl, [s_x4]
@@ -490,23 +495,23 @@ _verFuncion:
     int 21h
     
     ;imprimiendo coeficiente x0
-    mov ah, 02h
-    mov dl, [s_x0]
-    add dl, 30h
+    mov ah, 02h         ;Desplegando caracter en pantalla
+    mov dl, [s_x0]      ;que será lo que esté en la direccioń s_x0
+    add dl, 30h         ;sumandole 30h para que el usuario lo comprenda
     int 21h
 
-    mov al, [n_x0]
-    AAM
-    mov bx, ax
-    mov ah, 02h
-    mov dl, bh
-    add dl, 30h
-    int 21h
+    mov al, [n_x0]      ;moviendo lo que hay en el registro n_x0
+    AAM                 ;Instrucción de Desempaquetar
+    mov bx, ax          ;mover el registro ax en bx
+    mov ah, 02h         ;Desplegando un caracter en la pantalla, que será el primer dígito
+    mov dl, bh          ;Mover a dl lo que hay en dh
+    add dl, 30h         ;Sumando lo que hay en el registro dl un 30h para que el usuario pueda verlo como número
+    int 21h             ;Interrupción DOS
     
-    mov ah, 02h
-    mov dl, bl
-    add dl, 30h
-    int 21h
+    mov ah, 02h         ;Desplegará otro caracter en la pantalla, que será el segundo dígito       
+    mov dl, bl          ;Moviendo al registro dl lo que hay en el registro b en su parte baja
+    add dl, 30h         ;Se le sumará 30h para poder verse como número decimal
+    int 21h             ;Interrupción DOS
 
     call _lineaNueva
 
@@ -519,7 +524,7 @@ _verFuncion:
     call _lineaNueva
     call _lineaNueva
 
-    mov ah, 01h
+    mov ah, 01h         ;Esperando que el usuario presione 0 para volver al menú.
     int 21h
     cmp al, 30h             
     je _start
@@ -545,17 +550,17 @@ _imprimirDerivada:
     int 21h
 
     ;imprimir derivada de x5
-    mov al, [n_x5]
-    mov bl, 5
-    mul bl
-    mov [der_x5], al
+    mov al, [n_x5]      ;Mover al registro AL lo que hay en n_x5
+    mov bl, 5           ;Mover al registro bl el número 5
+    mul bl              ;Realizando multiplicación AL = AL*BL
+    mov [der_x5], al    ;El resultado (AL) se guarda en el contenido del registro der_x5
 
     mov ah, 02h
     mov dl, [s_x5]
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la derivada.
     mov al, [der_x5]
     AAM
     mov bx, ax
@@ -572,6 +577,8 @@ _imprimirDerivada:
     mov ah, 09h
     mov dx, Output10
     int 21h
+    
+    ;El procedimiento anterior se repite en los demás coeficientes, cambiando únicamente los valores.
 
     ;imprimir derivada de x4
     mov al, [n_x4]
@@ -584,7 +591,7 @@ _imprimirDerivada:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo
     mov al, [der_x4]
     AAM
     mov bx, ax
@@ -613,7 +620,7 @@ _imprimirDerivada:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo
     mov al, [der_x3]
     AAM
     mov bx, ax
@@ -642,7 +649,7 @@ _imprimirDerivada:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo
     mov al, [der_x2]
     AAM
     mov bx, ax
@@ -671,7 +678,7 @@ _imprimirDerivada:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo
     mov al, [der_x1]
     AAM
     mov bx, ax
@@ -693,7 +700,7 @@ _imprimirDerivada:
     mov dx, Output15
     int 21h
 
-    mov ah, 01h
+    mov ah, 01h     ;Esperando que el usuario presione 0 para volver al menú.
     int 21h
     cmp al, 30h             
     je _start
@@ -719,19 +726,20 @@ _imprimirIntegral:
     int 21h
 
 
-    xor ax, ax  ;limpiar registros
-    ;imprimiendo integral de x5
-    mov bl, 6
-    mov al, [n_x5]
-    div bl
-    mov [int_x5], al
+    xor ax, ax  ;limpiar registros para que la división no dé problemas.
 
-    mov ah, 02h
+    ;imprimiendo integral de x5
+    mov bl, 6           ;mover a bl el número 6
+    mov al, [n_x5]      ;mover a al el coeficiente de x5
+    div bl              ;instrucción de división AL = AL/BL
+    mov [int_x5], al    ;El resultado de la división (AL) se guarda en el contenido del registro int_x5
+
+    mov ah, 02h         ;Imprimiendo el signo.
     mov dl, [s_x5]
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x5]
     AAM
     mov bx, ax
@@ -749,6 +757,8 @@ _imprimirIntegral:
     mov dx, Output18     
     int 21h
 
+    ;El procedimiento anterior se repite para los demás coeficientes cambiando los valores.
+
     xor ax, ax  ;limpiar registros
     ;imprimiendo integral de x4
     mov bl, 5
@@ -761,7 +771,7 @@ _imprimirIntegral:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x4]
     AAM
     mov bx, ax
@@ -791,7 +801,7 @@ _imprimirIntegral:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x3]
     AAM
     mov bx, ax
@@ -821,7 +831,7 @@ _imprimirIntegral:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x2]
     AAM
     mov bx, ax
@@ -851,7 +861,7 @@ _imprimirIntegral:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x1]
     AAM
     mov bx, ax
@@ -881,7 +891,7 @@ _imprimirIntegral:
     add dl, 30h
     int 21h
 
-    ;mensaje
+    ;Desempaquetando e imprimiendo la integral.
     mov al, [int_x0]
     AAM
     mov bx, ax
@@ -915,21 +925,21 @@ _imprimirIntegral:
     mov dx, Output15
     int 21h
 
-    mov ah, 01h
+    mov ah, 01h         ;Esperando que el usuario presione 0 para volver al menú.
     int 21h
     cmp al, 30h             
     je _start
 
     ret
 
-_signoPositivo:
+_signoPositivo:         ;Este método se usaría para reconocer cuando se ingresa un signo positivo
     mov ah, 09h
     mov dx, positivo
     int 21h
 
     ret
 
-_signoNegativo:
+_signoNegativo:         ;Este método se usaría para reconocer cuando se ingresa un signo negativo
     mov ah, 09h
     mov dx, negativo
     int 21h
@@ -941,11 +951,11 @@ _signoNegativo:
 
     ret
 
-_noEsNumero:
+_noEsNumero:                ;Método que devuelve error si el usuario ingresa otra cosa que no sean números enteros
     call _lineaNueva
     call _lineaNueva
 
-    mov ah, 09h
+    mov ah, 09h            
     mov dx, Output20
     int 21h
 
@@ -963,8 +973,8 @@ _noEsNumero:
 
     ret
 
-_noDisponible:
-    call _lineaNueva
+_noDisponible:              ;Método que avisa cuando la opción ingresada aún no está disponible
+    call _lineaNueva    
     call _lineaNueva
 
     mov ah, 09h
@@ -988,7 +998,7 @@ _noDisponible:
 
     ret
 
-_Salir:
+_Salir:                 ;Método para salir del programa
     call _lineaNueva
     call _lineaNueva
 
@@ -1000,7 +1010,7 @@ _Salir:
 
     ret
 
-_opcionInvalida:
+_opcionInvalida:        ;Método que marca error cuando la opción ingresada no está contenida en el menú.
     call _lineaNueva
     call _lineaNueva
 
@@ -1026,19 +1036,19 @@ _opcionInvalida:
     ret
     
 
-_limpiarPantalla:
+_limpiarPantalla:       ;Este método se llama cuando se quiere hacer limpieza de pantalla.
     mov ah,00h             
     mov al, 03h
     int 10h
     ret
 
-_lineaNueva:
+_lineaNueva:            ;Este metodo se llama cuando se quiere imprimir una nueva linea.
     mov ah, 09h
     mov dx, NuevaLinea
     int 21h
     ret
 
-_Escape:
+_Escape:                ;Si el usuario presiona la tecla ESC desde el menú, el programa se cierra.
     int 20h
     ret
 
